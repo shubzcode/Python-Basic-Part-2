fe431ccdd4022cdd03020ed4bd22782ea7de8b9c
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:28:01 +0530

    Update temprature-2.py
    Comment added

--- 1.Mathematical/temprature-2.py
+++ 1.Mathematical/temprature-2.py
@@ -1,3 +1,3 @@
-celsius = float(input("Enter temperature in Celsius: "))
+celsius = float(input("Enter temperature in Celsius: "))# enter the value
 fahrenheit = (celsius * 9/5) + 32
 print("Temperature in Fahrenheit:", fahrenheit)
