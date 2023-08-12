a752ee73511c94c671426191bb0b6853873c7528
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:22:21 +0530

    Calculate the area and circumference of a circle given the radius
    circle-circumfrance-1.py

---
+++ 1.Mathematical/circle-circumfrance-1.py
@@ -0,0 +1,11 @@
+# Calculate the area and circumference of a circle given the radius
+import math
+
+def area_of_circle(radius):
+  return math.pi * radius ** 2
+
+def circumference_of_circle(radius):
+  return 2 * math.pi * radius
+
+print(area_of_circle(5))# value already given 
+print(circumference_of_circle(5))
