8ff3ba7e8d7a13fde438fcd4467737a94cc374d3
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:57:53 +0530

    Create kmtomileconverter.py

---
+++ basics/kmtomileconverter.py
@@ -0,0 +1,6 @@
+# Converting Kilometers to Miles:
+kilometers = float(input("Enter distance in kilometers: "))
+conversion_factor = 0.621371
+
+miles = kilometers * conversion_factor
+print("Distance in miles:", miles)
