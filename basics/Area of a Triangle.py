daed58a5f7ff8cc256a0c9cf0145eb894acb280a
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:53:02 +0530

    arithmetic operations
    arithmetic operations

---
+++ basics/Area of a Triangle.py
@@ -0,0 +1,5 @@
+base = float(input("Enter the length of the base: "))
+height = float(input("Enter the height: "))
+
+area = 0.5 * base * height
+print("Area of the triangle:", area)
---
+++ basics/Arithmetic Operations.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
