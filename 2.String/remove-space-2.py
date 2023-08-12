a1392ea60308baa983e13935304a1196a3b27100
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:46:18 +0530

    Update remove-space-2.py
    second method

---
+++ 2.String/remove-space-2.py
@@ -0,0 +1,5 @@
+# Remove Spaces from a String:
+input_string = input("Enter a string with spaces: ")
+string_without_spaces = input_string.replace(" ", "")
+
+print("String without spaces:", string_without_spaces)
