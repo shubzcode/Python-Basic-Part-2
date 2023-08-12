14561668f1417420469cfe5af7b003b8afa9c09b
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:38:05 +0530

    Update upper-lowercase-2.py
    user input

---
+++ 2.String/upper-lowercase-2.py
@@ -0,0 +1,9 @@
+# Convert a string to uppercase or lowercase:
+#user input 
+input_string = input("Enter a string: ")
+
+uppercase_string = input_string.upper()
+lowercase_string = input_string.lower()
+
+print("Uppercase:", uppercase_string)
+print("Lowercase:", lowercase_string)
