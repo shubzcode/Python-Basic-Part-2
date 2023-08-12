ed864b6a6da7979cb13170790eb5db9d0b4bba07
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:29:06 +0530

    Update temprature-2.py
    Comments added on code

--- 1.Mathematical/temprature-2.py
+++ 1.Mathematical/temprature-2.py
@@ -1,3 +1,4 @@
+# ***Convert Temperature from Celsius to Fahrenheit:***
 celsius = float(input("Enter temperature in Celsius: "))# enter the value
 fahrenheit = (celsius * 9/5) + 32
 print("Temperature in Fahrenheit:", fahrenheit)
