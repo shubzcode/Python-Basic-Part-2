576192918fe253772e3fb4e1a6df33639fba2562
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:11:54 +0530

    update both method

---
+++ 1.Mathematical/sqareroot-1.py
@@ -0,0 +1,5 @@
+def square_root(number):
+  import math
+  return math.sqrt(number)
+#A
+print(square_root(16))
---
+++ 1.Mathematical/sqareroot-2.py
@@ -0,0 +1,5 @@
+import math
+#BY user input
+number = float(input("Enter a number: "))
+square_root = math.sqrt(number)
+print("Square root:", square_root)
