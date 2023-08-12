6efcaa6c57fa93e00b504b75a6e4f20729fcbd76
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-13 00:36:07 +0530

    method 1

---
+++ 2.String/upper-lowercase-1.py
@@ -0,0 +1,11 @@
+# Convert a string to uppercase or lowercase:
+def to_uppercase(string):
+    return string.upper()
+
+
+def to_lowercase(string):
+    return string.lower()
+
+
+print(to_uppercase("hello world"))
+print(to_lowercase("HELLO WORLD"))
